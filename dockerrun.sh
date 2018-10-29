2e80ce64cb9b44a8bbbc7585320bfb91a542b5a8
Mikhail <me@chuvash.pw>
2018-10-29 17:37:11 +0300

    Delete dockerrun.sh

--- dockerrun.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --name ubot --mount type=volume,source=ubotvk-log,dst=/app/log/ --mount type=volume,source=ubotvk-data,dst=/app/data/ --restart unless-stopped ubotvk
